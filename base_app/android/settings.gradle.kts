faf74a8a8e9e98807482ebfc4d35da0409cd67d4
Ryoji Tatebe <>
2025-07-18 10:23:58 +0900

    fix setting gradle

--- base_app/android/settings.gradle.kts
+++ base_app/android/settings.gradle.kts
@@ -1,15 +1,14 @@
-val flutterSdkPath = run {
-    val properties = java.util.Properties()
-    file("local.properties").inputStream().use { properties.load(it) }
-    val flutterSdkPath = properties.getProperty("flutter.sdk")
-    require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-    println("DEBUG: flutterSdkPath from local.properties: $flutterSdkPath")
-    flutterSdkPath
-}
-includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
 pluginManagement {
-
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+        println("DEBUG: flutterSdkPath from local.properties: $flutterSdkPath")
+        flutterSdkPath
+    }
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
         google()
@@ -19,6 +18,7 @@ pluginManagement {
 }
 
 plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0" apply true
     id("com.android.application") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
 }
